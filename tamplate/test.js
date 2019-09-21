4337ccecffaad1ace93d938755a5e15eb85c6cbc
qshfu0099 <55616558+qshfu0099@users.noreply.github.com>
2019-09-21 16:53:08 +0800

    Create test.js

---
+++ tamplate/test.js
@@ -0,0 +1,23 @@
+html = '%3Cdiv%20class=%22t1%22%3E%0D%0A' +
+    '%20%20%20%20%3Cp%3E-%7B%7Binput.value%7D%7D-%3C/p%3E%0D%0A' +
+    '%20%20%20%20%3Cdiv%20style=%22color:%20white%22%3E%0D%0A' +
+    '%20%20%20%20%20%20%20%20%3Cslot/%3E%0D%0A' +
+    '%20%20%20%20%3C/div%3E%0D%0A' +
+    '%20%20%20%20666%0D%0A' +
+    '%20%20%20%20%3Cdiv%20style=%22color:%20white%22%3E%0D%0A' +
+    '%20%20%20%20%20%20%20%20%3Cslot/%3E%0D%0A' +
+    '%20%20%20%20%3C/div%3E%0D%0A' +
+    '%20%20%20%20%3Cslot%20all%3E%3C/slot%3E%0D%0A' +
+    '%3C/div%3E%0D%0A' +
+    '%3Cstyle%3E%0D%0A' +
+    '%20%20%20%20.&input%20%7B%0D%0A' +
+    '%20%20%20%20%20%20%20%20border-bottom:%201px%20solid%20gray;%0D%0A' +
+    '%20%20%20%20%7D%0D%0A' +
+    '%3C/style%3E%0D%0A' +
+    '%3Cdiv%20class=%22&input%22%3E%0D%0A' +
+    '%20%20%20%20%3Clabel%3E%0D%0A' +
+    '%20%20%20%20%20%20%20%20%7Blabel%7D%EF%BC%9A%0D%0A' +
+    '%20%20%20%20%20%20%20%20%3Cinput%20name=%7Bname%7D%20value=%7Bvalue%7D%20/%3E%0D%0A' +
+    '%20%20%20%20%3C/label%3E%0D%0A' +
+    '%3C/div%3E%0D%0A';
+decode = true;
